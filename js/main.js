3b4fec6dd34c0b892a84bf989c3d9775522d1756
Taylor Radtke <taylorradtke@gmail.com>
2017-04-05 14:28:45 -0400

    added sponser trail version

--- js/main.js
+++ js/main.js
@@ -15,6 +15,7 @@
       init_classic_menu();
       video_bg_init();
       initPageSliders();
+      initSponsersSort();
 
   });
 
@@ -610,6 +611,30 @@ function initPageSliders(){
             slidesToScroll: 1
           }
         }]
+      });
+
+
+    })(jQuery);
+};
+
+function initSponsersSort(){
+    (function($){
+        "use strict";
+
+      var __sf = $(".sponsorsFooter ul span");
+      var __sf_cont = $(".sponsorsFooter ul");
+      var __title = $("#sponsorTitle");
+
+      __sf.mouseenter(function() {
+        $(this).addClass('hover');
+        __sf_cont.addClass('enter');
+        var __setTitle = $(this).attr("data-group-title");
+        __title.html(__setTitle);
+      }).mouseleave(function() {
+        $(this).removeClass('hover');
+        __sf_cont.removeClass('enter');
+        var __setTitle = $(__title).attr("data-default-title");
+        __title.html(__setTitle);
       });
 
 
