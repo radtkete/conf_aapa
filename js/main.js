ff1a798df11c8654ff7688713b743f4a9b0d84c1
Taylor Radtke <taylorradtke@gmail.com>
2017-04-05 14:08:30 -0400

    events tidy

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@
       init_mobile_nav_toggle();
       init_classic_menu();
       video_bg_init();
+      initPageSliders();
 
   });
 
@@ -574,3 +575,43 @@ function parallax(){
 
 
 
+/* ---------------------------------------------
+ Sliders
+ --------------------------------------------- */
+function initPageSliders(){
+    (function($){
+        "use strict";
+
+      // Landing Page - Blog Posts Slider
+      $(".special-events__carousel").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: "<button class='owl-prev'><i class='fa fa-angle-left'></i></button>",
+        nextArrow: "<button class='owl-next'><i class='fa fa-angle-right'></i></button>",
+        responsive: [{
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+            infinite: true,
+            dots: true
+          }
+        }, {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2
+          }
+        }, {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+      });
+
+
+    })(jQuery);
+};
